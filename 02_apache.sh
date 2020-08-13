28651dbb7206a81cd1e9b1046cffa5a429fd3567
Dave Leach <dave@davedoeswork.com>
2020-08-13 12:30:01 -0700

    Added code to make the Apache user and group the owner of the document root

--- 02_apache.sh
+++ 02_apache.sh
@@ -80,6 +80,9 @@ fi
 # Set the global ServerName if needed
 declare -r apache_global_conf="/etc/apache2/apache2.conf";
 
+# Enable mod_rewrite
+a2enmod rewrite;
+
 if ! grep -q ServerName $apache_global_conf;
 then
   echo "Setting global ServerName to localdev"
@@ -98,6 +101,57 @@ a2ensite -q $virtual_host_name;
 # Restart the Apache service
 echo "Restarting Apache";
 service apache2 restart;
+
+# Get the user and group that Apache is running as
+declare apache_config=$(apachectl -t -D DUMP_RUN_CFG);
+
+declare user_regex="User\:[[:space:]]name=\"([A-Za-z0-9\-]+)\"";
+declare apache_user;
+
+if [[ $apache_config =~ $user_regex ]];
+then
+  apache_user=${BASH_REMATCH[1]};
+  echo "Apache user is $apache_user";
+else
+  echo "Can't find Apache user";
+fi
+
+declare group_regex="Group\:[[:space:]]name=\"([A-Za-z0-9\-]+)\"";
+declare apache_group;
+
+if [[ $apache_config =~ $group_regex ]];
+then
+  apache_group=${BASH_REMATCH[1]};
+  echo "Apache group is $apache_group";
+else
+  echo "Can't find Apache group";
+fi
+
+if [[ ! -z $apache_user && ! -z $apache_group ]];
+then
+  # Make the Apache user and group the owner of the document root
+  echo "Making $apache_user:$apache_group the owner of $virtual_host_document_root_path";
+  chown $apache_user:$apache_group -R $virtual_host_document_root_path;
+
+  # Make the document root writable for the Apache user's group
+  echo "Making $virtual_host_document_root_path writable for $apache_group group";
+  chmod g+w -R $virtual_host_document_root_path;
+fi
+
+# Check to see if the current user is part of the Apache user's group
+declare current_user="${SUDO_USER:-${USER}}";
+declare current_user_groups=$(groups $current_user);
+
+# TO-DO: Make this resilient to bad replies
+if [[ ! $current_user_groups =~ $apache_group ]];
+then
+  read -p "Add $current_user to $apache_group group? " user_input;
+
+  if [ $user_input = "y" ];
+  then
+    usermod -a -G $apache_group $current_user;
+  fi
+fi
 
 # Remind the user to add an entry to the hosts file
 echo "Add the following line to /etc/hosts if needed";
